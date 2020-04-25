59300fe5442d84e5679d68333f3b21fcb6317e18
Vlad Cuciureanu <vlad.c.cuciureanu@gmail.com>
2020-04-25 23:52:55 +0300

    Restructured project files and started implementing tests

---
+++ tests/Tests.cpp
@@ -0,0 +1,11 @@
+#include <Medicine.hpp>
+#include <Employee.hpp>
+#include <RepoSTLTemplate.hpp>
+#include <FileRepoSTLTemplate.hpp>
+
+int main()
+{
+    //TODO: tests here
+    std::cout<<"Passed all tests successfully!";
+    return 0;
+}
